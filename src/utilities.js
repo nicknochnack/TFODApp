264431593b0bdc4e2ff9ce29c007a098ba763986
Armaan Salam <38160090+ArmaanAS@users.noreply.github.com>
2021-05-12 20:11:50 +0100

    Fix bounding box rect drawing
    The width and height variables were named incorrectly. They are actually the x, y position of the bottom right. Also this fixes the bounding box, canvas rect, drawing.

--- src/utilities.js
+++ src/utilities.js
@@ -11,7 +11,7 @@ export const drawRect = (boxes, classes, scores, threshold, imgWidth, imgHeight,
     for(let i=0; i<=boxes.length; i++){
         if(boxes[i] && classes[i] && scores[i]>threshold){
             // Extract variables
-            const [y,x,height,width] = boxes[i]
+            const [y,x,b,a] = boxes[i]
             const text = classes[i]
             
             // Set styling
@@ -23,7 +23,7 @@ export const drawRect = (boxes, classes, scores, threshold, imgWidth, imgHeight,
             // DRAW!!
             ctx.beginPath()
             ctx.fillText(labelMap[text]['name'] + ' - ' + Math.round(scores[i]*100)/100, x*imgWidth, y*imgHeight-10)
-            ctx.rect(x*imgWidth, y*imgHeight, width*imgWidth/2, height*imgHeight/2);
+            ctx.rect(x*imgWidth, y*imgHeight, (a-x)*imgWidth, (b-y)*imgHeight);
             ctx.stroke()
         }
     }
